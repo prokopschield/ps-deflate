30018fa248e04a9ccecfd560520bbbaab34225a1
Prokop <prokop.schield@protonmail.com>
2025-01-03 20:58:13 +0100

    refactor: use thread_local! for compressor

--- src/lib.rs
+++ src/lib.rs
@@ -1,119 +1,46 @@
 mod error;
 
 pub use error::PsDeflateError;
+
+use std::cell::RefCell;
+
+use libdeflater::{CompressionLvl, Compressor, Decompressor};
 use ps_buffer::Buffer;
 
-use std::cell::Cell;
-
-fn alloc_compressor() -> libdeflater::Compressor {
-    let level = libdeflater::CompressionLvl::best();
-
-    libdeflater::Compressor::new(level)
+thread_local! {
+    pub static COMPRESSOR: RefCell<Compressor> = RefCell::from(Compressor::new(CompressionLvl::best()));
+    pub static DECOMPRESSOR: RefCell<Decompressor> = RefCell::from(Decompressor::new());
 }
 
-fn get_compressor(cell: &Cell<Option<libdeflater::Compressor>>) -> libdeflater::Compressor {
-    match cell.replace(None) {
-        Some(compressor) => compressor,
-        None => alloc_compressor(),
-    }
+pub fn compress_into(data: &[u8], out_data: &mut [u8]) -> Result<usize, PsDeflateError> {
+    COMPRESSOR.with(|c| Ok(c.borrow_mut().deflate_compress(data, out_data)?))
 }
 
-fn put_compressor(
-    cell: &Cell<Option<libdeflater::Compressor>>,
-    compressor: libdeflater::Compressor,
-) {
-    cell.replace(Some(compressor));
+pub fn compress(data: &[u8]) -> Result<Buffer, PsDeflateError> {
+    let out_size = data.len() + 5;
+    let mut out_data = Buffer::alloc(out_size);
+
+    let size = compress_into(data, &mut out_data)?;
+
+    if size < out_size {
+        out_data.resize(size);
+    }
+
+    Ok(out_data)
 }
 
-fn alloc_decompressor() -> libdeflater::Decompressor {
-    libdeflater::Decompressor::new()
+pub fn decompress_into(data: &[u8], out_data: &mut [u8]) -> Result<usize, PsDeflateError> {
+    DECOMPRESSOR.with(|d| Ok(d.borrow_mut().deflate_decompress(data, out_data)?))
 }
 
-fn get_decompressor(cell: &Cell<Option<libdeflater::Decompressor>>) -> libdeflater::Decompressor {
-    match cell.replace(None) {
-        Some(compressor) => compressor,
-        None => alloc_decompressor(),
-    }
-}
+pub fn decompress(data: &[u8], out_size: usize) -> Result<Buffer, PsDeflateError> {
+    let mut out_data = Buffer::alloc(out_size);
 
-fn put_decompressor(
-    cell: &Cell<Option<libdeflater::Decompressor>>,
-    decompressor: libdeflater::Decompressor,
-) {
-    cell.replace(Some(decompressor));
-}
+    let size = decompress_into(data, &mut out_data)?;
 
-pub struct Compressor {
-    compressor: Cell<Option<libdeflater::Compressor>>,
-    decompressor: Cell<Option<libdeflater::Decompressor>>,
-}
-
-impl Compressor {
-    pub fn new() -> Self {
-        Self {
-            compressor: Cell::from(None),
-            decompressor: Cell::from(None),
-        }
+    if size < out_size {
+        out_data.resize(size);
     }
 
-    pub fn compress_into(&self, data: &[u8], out_data: &mut [u8]) -> Result<usize, PsDeflateError> {
-        let mut compressor = get_compressor(&self.compressor);
-
-        let result = compressor.deflate_compress(data, out_data);
-
-        put_compressor(&self.compressor, compressor);
-
-        Ok(result?)
-    }
-
-    pub fn compress(&self, data: &[u8]) -> Result<Buffer, PsDeflateError> {
-        let out_size = data.len() + 5;
-        let mut out_data = Buffer::alloc(out_size);
-
-        let size = self.compress_into(data, &mut out_data)?;
-
-        if size < out_size {
-            out_data.resize(size);
-        }
-
-        Ok(out_data)
-    }
-
-    pub fn decompress_into(
-        &self,
-        data: &[u8],
-        out_data: &mut [u8],
-    ) -> Result<usize, PsDeflateError> {
-        let mut decompressor = get_decompressor(&self.decompressor);
-
-        let result = decompressor.deflate_decompress(data, out_data);
-
-        put_decompressor(&self.decompressor, decompressor);
-
-        Ok(result?)
-    }
-
-    pub fn decompress(&self, data: &[u8], out_size: usize) -> Result<Buffer, PsDeflateError> {
-        let mut out_data = Buffer::alloc(out_size);
-
-        let size = self.decompress_into(data, &mut out_data)?;
-
-        if size < out_size {
-            out_data.resize(size);
-        }
-
-        Ok(out_data)
-    }
-}
-
-impl Clone for Compressor {
-    fn clone(&self) -> Self {
-        Self::new()
-    }
-}
-
-impl Default for Compressor {
-    fn default() -> Self {
-        Self::new()
-    }
+    Ok(out_data)
 }
